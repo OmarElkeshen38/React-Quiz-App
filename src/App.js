0a88403ba27a420f2e02f47ed00fd85bc7ce639c
OmarElkeshen38 <oelkeshen5@gmail.com>
2023-10-27 03:07:48 +0200

    load data from fake api (json-server)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import DataCounter from "./DateCounter";
+import React, { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
 
-function App() {
-  return (
-    <>
-      <DataCounter />
-    </>
-  )
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', active', 'finished'
+  status: 'loading',
 }
 
-export default App
+function reducer(state, action) {
+  switch(action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      }
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
+function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
+}
+
+export default App;
