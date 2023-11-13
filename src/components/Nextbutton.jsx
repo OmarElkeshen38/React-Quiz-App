35b8b186b0c03ef95eddc3a45919d292deb88322
OmarElkeshen38 <oelkeshen5@gmail.com>
2023-11-13 06:06:06 +0200

    add restarting a quiz feature

--- src/components/Nextbutton.jsx
+++ src/components/Nextbutton.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
-function Nextbutton({ dispatch, answer}) {
+function Nextbutton({ dispatch, answer, index, numQuestion }) {
 
-    if(answer === null) return null;
+  if(answer === null) return null;
 
-  return (
+  if(index < numQuestion -1) return (
     <button className='btn btn-btn-ui'
-    on onClick={() => dispatch({ type: "nextQuestion" })}>
+    onClick={() => dispatch({ type: "nextQuestion" })}>
         Next
     </button>
   )
+
+  if(index === numQuestion -1) return (
+    <button
+      className="btn btn-btn-ui"
+      onClick={() => dispatch({ type: "finish" })}
+    >
+      Finish
+    </button>
+  );
 }
 
 export default Nextbutton
