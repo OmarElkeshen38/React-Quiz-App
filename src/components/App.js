75a27f8c4622f83a5395148d2114856fc6fbbff7
OmarElkeshen38 <oelkeshen5@gmail.com>
2023-11-20 05:53:17 +0200

    set up timer with useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,10 @@ import Question from "./Question";
 import Nextbutton from "./Nextbutton";
 import Progress from "./Progress";
 import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+
+const SECS_PER_QUESTION = 30;
 
 const initialState = {
   questions: [],
@@ -18,6 +22,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -37,6 +42,7 @@ function reducer(state, action) {
       return {
         ...state,
         status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
       };
     case "newAnswer":
       const question = state.questions.at(state.index);
@@ -55,13 +61,16 @@ function reducer(state, action) {
         return { ...state, status:"finished", highscore: state.points > state.highscore ? state.points : state.highscore };
       case "restart":
         return { ...initialState, questions: state.questions, status: "ready" }
+      case "tick":
+        return { ...state, secondsRemaining: state.secondsRemaining - 1,
+                status: state.secondsRemaining === 0 ? "finished" : state.status }
         default:
       throw new Error("Action unknown");
   }
 }
 
 function App() {
-  const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points, highscore, secondsRemaining }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -103,12 +112,16 @@ function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <Nextbutton
-              dispatch={dispatch}
-              answer={answer}
-              numQuestion={numQuestions}
-              index={index}
-            />
+
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <Nextbutton
+                dispatch={dispatch}
+                answer={answer}
+                numQuestion={numQuestions}
+                index={index}
+              />
+            </Footer>
           </>
         )}
 
--- src/components/Nextbutton.jsx
+++ src/components/Nextbutton.jsx
@@ -5,7 +5,7 @@ function Nextbutton({ dispatch, answer, index, numQuestion }) {
   if(answer === null) return null;
 
   if(index < numQuestion -1) return (
-    <button className='btn btn-btn-ui'
+    <button className='btn btn-ui'
     onClick={() => dispatch({ type: "nextQuestion" })}>
         Next
     </button>
